5249f5a1d38798092a7a2e3b68fae53a05416908
Umidjonov <umidjonov.azizbek@gmail.com>
2020-11-07 23:33:24 +0500

    getting instance varriable

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,20 @@
-import copy
+import copy as cp
 import random
+
+
 # Consider using the modules imported above.
 
 class Hat:
+    contents = []
 
+    def __init__(self, **kwargs):
+        print(f"'QWERTY'{kwargs}")
+        for ball in kwargs.items():
+            key, val = ball
+            # print(ball)
+            for i in range(val):
+                self.contents.append(key)
+        print(f"'QWERTY'{self.contents}")
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
